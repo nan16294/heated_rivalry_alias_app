a2979019e9be253f7907cf53936791e0dd22a502
nan16294 <nan16294@gmail.com>
2026-06-05 22:58:47 +0400

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,41 @@
+export const WORDS_WEBHOOK_URL = "https://hook.eu2.make.com/wf4t6yfunla0b2k7jdo0rubu09obr6ft";
+
+export const ROUND_SECONDS = 90;
+
+// Обои кладём в public/backgrounds с такими именами.
+// Если файла пока нет, приложение просто покажет градиентный фон.
+export const BACKGROUNDS = {
+  home: "/backgrounds/home.jpg",
+  teams: "/backgrounds/teams.jpg",
+  round: "/backgrounds/round.jpg",
+  play: "/backgrounds/play.jpg",
+};
+
+export const FALLBACK_WORDS = [
+  "Шейн Холландер",
+  "Илья Розанов",
+  "соперничество",
+  "хоккей",
+  "раздевалка",
+  "арена",
+  "тайная встреча",
+  "Канада",
+  "Россия",
+  "клюшка",
+  "шайба",
+  "тренировка",
+  "финал",
+  "матч",
+  "плей-офф",
+  "капитан",
+  "команда",
+  "форма",
+  "коньки",
+  "поездка",
+  "отель",
+  "поцелуй",
+  "интервью",
+  "фанаты",
+  "победа",
+  "поражение"
+];
